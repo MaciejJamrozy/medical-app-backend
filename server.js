56e57976e4b2ef9ae18999e52eedb679b613379a
Maciej Jamroży <maciej@Maciejs-MacBook-Pro.local>
2026-01-17 12:55:35 +0100

    12

--- server.js
+++ server.js
@@ -15,7 +15,10 @@ app.use(express.json());
 
 const server = http.createServer(app);
 const io = new Server(server, {
-    cors: { origin: "http://localhost:3000", methods: ["GET", "POST"] }
+  cors: { 
+    origin: ["http://localhost:3000", "http://localhost:5173"], 
+    methods: ["GET", "POST"] 
+  }
 });
 
 app.use((req, res, next) => {
